be30ee857ccc8a0ec19e2a26f7c175cfd6035a98
Jason Schmitt <schmitty890@gmail.com>
2020-11-18 20:37:22 -0500

    adding prettier so every time we save files get formatted

--- src/App.spec.js
+++ src/App.spec.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react'
 import App from './App'
 
 describe('App', () => {
-    it('Renders without error', () => {
-        render(<App/>)
-    })
+  it('Renders without error', () => {
+    render(<App />)
+  })
 })
