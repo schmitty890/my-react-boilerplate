a2d4936f785fe10fef03e9308d5e1f0964a0e039
Jason Schmitt <schmitty890@gmail.com>
2020-11-18 20:05:51 -0500

    '

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,5 +1,10 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
+import '@testing-library/jest-dom'
+
 describe('App', () => {
-    it('Runs and passes', () => {
-        expect(true).toBe(true)
+    it('Renders without error', () => {
+        render(<App/>)
     })
 })
