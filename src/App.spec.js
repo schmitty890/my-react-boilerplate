b04e1947b30aadc37c68d682b084c18d7ecba05e
Jason Schmitt <schmitty890@gmail.com>
2020-11-18 20:17:08 -0500

    adding jest config to import imports automatically

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
-import '@testing-library/jest-dom'
 
 describe('App', () => {
     it('Renders without error', () => {
